c3a4fea24d9f2ffd353e57611ee9531e3a4b8165
Michael Davis <mhdavis1993@gmail.com>
2018-03-20 10:50:54 -0400

    Added fields to Vehicle class
    - Added name, make and model fields as strings
    - Added constraints to each field

--- grails-app/domain/grails/app/tutorial/Vehicle.groovy
+++ grails-app/domain/grails/app/tutorial/Vehicle.groovy
@@ -2,6 +2,14 @@ package grails.app.tutorial
 
 class Vehicle {
 
+    String name
+
+    String make
+    String model
+
     static constraints = {
+      name maxSize: 255
+      make inList: ['Ford', 'Chevrolet', 'Nissan']
+      model nullable: true
     }
 }
